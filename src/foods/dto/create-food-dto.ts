c793a5ddccc2b7cde3f7101a6f9bffe563d36d7e
takato <ltakato@hotmail.com>
2019-09-05 18:03:40 -0300

    feat: add food request body validation

--- src/foods/dto/create-food-dto.ts
+++ src/foods/dto/create-food-dto.ts
@@ -1,3 +1,6 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateFoodDto {
+  @IsNotEmpty()
   readonly name: string;
 };
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
+
 bootstrap();
